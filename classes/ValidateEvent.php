61f2ca7d0b416bb52179eb09aa84d016833a8728
Paul Lovato <plovato@gmail.com>
2019-03-19 12:21:04 -0600

    Updated ValidateEvent to support custom messages.

--- classes/ValidateEvent.php
+++ classes/ValidateEvent.php
@@ -6,21 +6,27 @@ use Cleanse\Event\Classes\Helpers\FactoryHelper;
 
 class ValidateEvent
 {
-    private $default;
+    private $rules;
     private $type;
 
     public function __construct()
     {
-        $this->default = [
-            'event-title' => 'required',
-            'event-type' => 'required'
+        $this->rules = [
+            'validation' => [
+                'event-title' => 'required',
+                'event-type'  => 'required'
+            ],
+            'messages' => [
+                'event-title.required' => 'Please enter a title for your event.',
+                'event-type.required'  => 'Please select an event type for your event.'
+            ]
         ];
     }
 
     public function validateEvent($eventType, $namespace)
     {
         if (!$eventType == '') {
-            $this->type = ((new FactoryHelper)->getInstance($namespace, $eventType))->rules();
+            $this->type = ((new FactoryHelper)->getInstance($namespace, $eventType))->validation();
         }
 
         return $this->mergeConfigs();
@@ -28,7 +34,11 @@ class ValidateEvent
 
     private function mergeConfigs()
     {
-        $this->type = isset($this->type) ? $this->type : [];
-        return array_merge($this->default, $this->type);
+        $typeRules = isset($this->type) ? $this->type : ['validation' => [], 'messages' => []];
+
+        return [
+            'validation' => array_merge($this->rules['validation'], $typeRules['validation']),
+            'messages' => array_merge($this->rules['messages'], $typeRules['messages'])
+        ];
     }
 }
