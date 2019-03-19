245f0a3b512614673dc3706b5ea0f024a1ab9067
Paul Lovato <plovato@gmail.com>
2019-03-18 18:19:28 -0600

    Adjustment to RR generator.

--- classes/generators/RoundRobin.php
+++ classes/generators/RoundRobin.php
@@ -4,6 +4,10 @@ namespace Cleanse\Event\Classes\Generators;
 
 use Exception;
 
+use Cleanse\Event\Classes\Helpers\RoundRobinHelper;
+use Cleanse\Event\Models\Event;
+use Cleanse\Event\Models\Match;
+
 class RoundRobin
 {
     public $teams;
@@ -25,15 +29,27 @@ class RoundRobin
     public function generate()
     {
         if (count($this->teams) <= 1) {
+            //Will be handled already.
             throw new Exception('You need to specify at least 2 teams.');
         }
 
         return $this->makeSchedule();
     }
 
-    public function create($schedule)
+    public function create($event)
     {
-        return 'Do database stuff.';
+        $newEvent = new Event;
+
+        $newEvent->name = $event['name'];
+        $newEvent->description = $event['description'];
+        $newEvent->event_type = $event['event_type'];
+
+        $newEvent->save();
+
+        //return $newEvent;
+        //create Matches
+
+        return 'Add teams.';
     }
 
     private function makeSchedule()
@@ -43,7 +59,7 @@ class RoundRobin
         }
 
         //Divide teams into groups.
-        $groupTeams = $this->array_partition($this->teams, $this->groups);
+        $groupTeams = RoundRobinHelper::array_partition($this->teams, $this->groups);
 
         $matches = [];
         foreach ($groupTeams as $group) {
@@ -53,7 +69,9 @@ class RoundRobin
         return $matches;
     }
 
-    /** Thanks https://en.wikipedia.org/wiki/Round-robin_tournament#Scheduling_algorithm */
+    /**
+     * Thanks https://en.wikipedia.org/wiki/Round-robin_tournament#Scheduling_algorithm
+     */
     private function generateGroupMatches($teams)
     {
         //if there is 0-1 teams in this group, error so a balanced group:team ratio is met.
@@ -97,72 +115,10 @@ class RoundRobin
                 $matches[] = $match;
             }
 
-            $this->rotate($teams);
+            RoundRobinHelper::rotate($teams);
         }
 
         /** Number of matches =【 N ×（ N-1 ) 】÷ 2 */
         return $matches;
-    }
-
-    /** Thanks https://gist.github.com/mcrumley/10396818 */
-    private function array_partition(array $a, $np = 1, $pad = false)
-    {
-        $np = (int)$np;
-        if ($np <= 0) {
-            throw new Exception('Array partition count must be greater than zero.');
-        }
-
-        $c = count($a);
-        $per_array = (int)floor($c / $np);
-        $rem = $c % $np;
-
-        if ($c === 0) {
-            if ($pad) {
-                $result = array_fill(0, $np, array());
-            } else {
-                $result = array();
-            }
-        } elseif ($rem === 0 || $rem == $np - 1 || $np >= $c) {
-            $result = array_chunk($a, $per_array + ($rem > 0 ? 1 : 0));
-
-            if ($pad && $np > $c) {
-                $result = array_merge($result, array_fill(0, $np - $c, array()));
-            }
-        } else {
-            $split = $rem * ($per_array + 1);
-            $result = array_chunk(array_slice($a, 0, $split), $per_array + 1);
-            $result = array_merge($result, array_chunk(array_slice($a, $split), $per_array));
-        }
-
-        return $result;
-    }
-
-    /** Thanks to 'mnito'. */
-    private function rotate(array &$items)
-    {
-        $itemCount = count($items);
-        if ($itemCount < 3) {
-            return;
-        }
-        $lastIndex = $itemCount - 1;
-
-        /**
-         * Though not technically part of the round-robin algorithm, odd-even
-         * factor differentiation included to have intuitive behavior for arrays
-         * with an odd number of elements
-         */
-        $factor = (int)($itemCount % 2 === 0 ? $itemCount / 2 : ($itemCount / 2) + 1);
-        $topRightIndex = $factor - 1;
-        $topRightItem = $items[$topRightIndex];
-        $bottomLeftIndex = $factor;
-        $bottomLeftItem = $items[$bottomLeftIndex];
-        for ($i = $topRightIndex; $i > 0; $i -= 1) {
-            $items[$i] = $items[$i - 1];
-        }
-        for ($i = $bottomLeftIndex; $i < $lastIndex; $i += 1) {
-            $items[$i] = $items[$i + 1];
-        }
-        $items[1] = $bottomLeftItem;
-        $items[$lastIndex] = $topRightItem;
     }
 }
---
+++ classes/helpers/RoundRobinHelper.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Cleanse\Event\Classes\Helpers;
+
+class RoundRobinHelper
+{
+    /**
+     * Thanks https://gist.github.com/mcrumley/10396818
+     */
+    public static function array_partition(array $a, $np = 1, $pad = false)
+    {
+        $np = (int)$np;
+        if ($np <= 0) {
+            trigger_error('partition count must be greater than zero', E_USER_NOTICE);
+            return array();
+        }
+
+        $c = count($a);
+        $per_array = (int)floor($c / $np);
+        $rem = $c % $np;
+
+        if ($c === 0) {
+            if ($pad) {
+                $result = array_fill(0, $np, array());
+            } else {
+                $result = array();
+            }
+        } elseif ($rem === 0 || $rem == $np - 1 || $np >= $c) {
+            $result = array_chunk($a, $per_array + ($rem > 0 ? 1 : 0));
+
+            if ($pad && $np > $c) {
+                $result = array_merge($result, array_fill(0, $np - $c, array()));
+            }
+        } else {
+            $split = $rem * ($per_array + 1);
+            $result = array_chunk(array_slice($a, 0, $split), $per_array + 1);
+            $result = array_merge($result, array_chunk(array_slice($a, $split), $per_array));
+        }
+
+        return $result;
+    }
+
+    /**
+     * Thanks to 'mnito'.
+     */
+    public static function rotate(array &$items)
+    {
+        $itemCount = count($items);
+        if ($itemCount < 3) {
+            return;
+        }
+        $lastIndex = $itemCount - 1;
+
+        /**
+         * Though not technically part of the round-robin algorithm, odd-even
+         * factor differentiation included to have intuitive behavior for arrays
+         * with an odd number of elements
+         */
+        $factor = (int)($itemCount % 2 === 0 ? $itemCount / 2 : ($itemCount / 2) + 1);
+        $topRightIndex = $factor - 1;
+        $topRightItem = $items[$topRightIndex];
+        $bottomLeftIndex = $factor;
+        $bottomLeftItem = $items[$bottomLeftIndex];
+        for ($i = $topRightIndex; $i > 0; $i -= 1) {
+            $items[$i] = $items[$i - 1];
+        }
+        for ($i = $bottomLeftIndex; $i < $lastIndex; $i += 1) {
+            $items[$i] = $items[$i + 1];
+        }
+        $items[1] = $bottomLeftItem;
+        $items[$lastIndex] = $topRightItem;
+    }
+}
