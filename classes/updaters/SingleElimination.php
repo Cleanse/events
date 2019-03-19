1e8c9e24d02f25b8da6282b61c599cef54ec434b
Paul Lovato <plovato@gmail.com>
2019-03-18 18:18:38 -0600

    Updater namespace.

---
+++ classes/updaters/SingleElimination.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Cleanse\Event\Classes\Updaters;
+
+use Cleanse\Event\Models\Event;
+use Cleanse\Event\Models\Match;
+
+class SingleElimination
+{
+    public function __construct($config)
+    {
+    }
+
+    public function update()
+    {
+    }
+}
