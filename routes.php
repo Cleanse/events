fda2d4807f05d8215e095e3a5b8b1e7c9651cb25
Paul Lovato <plovato@gmail.com>
2019-05-03 19:04:54 -0600

    API for 'bracket' events.

--- routes.php
+++ routes.php
@@ -5,7 +5,8 @@ use Cleanse\Event\Models\Match;
 use Cleanse\Event\Models\Team;
 use Cleanse\Event\Models\Broadcast;
 
-Route::get('/api/broadcast/{broadcast}/match', function ($broadcastId) {
+Route::get('/api/broadcast/{broadcast}/match', function ($broadcastId)
+{
     $broadcast = Broadcast::where(['id' => $broadcastId])
     ->first();
 
@@ -20,85 +21,27 @@ Route::get('/api/broadcast/{broadcast}/match', function ($broadcastId) {
     return Response::json([]);
 })->where(['broadcast' => '[0-9]+']);
 
-//Route::get('/api/broadcast/schedule', function () {
-//    $event = Event::whereNull('completed')
-//        ->orWhere('completed', 0)
-//        ->with(['matches' => function ($q) {
-//            $q->select('event_id', 'match_id', 'claws_score', 'fangs_score');
-//            $q->orderBy('order', 'asc');
-//        }])
-//        ->orderBy('id', 'asc')
-//        ->first();
-//
-//    return Response::json($event);
-//});
-//
-//Route::get('/api/broadcast/score  ', function () {
-//    $event = Event::whereNull('completed')
-//        ->orWhere('completed', 0)
-//        ->orderBy('id', 'asc')
-//        ->first();
-//
-//    if (isset($event->active_match)) {
-//        $match = Match::whereId($event->active_match)
-//            ->with(['claws.team.logo', 'fangs.team.logo'])
-//            ->first();
-//
-//        return Response::json($match);
-//    }
-//
-//    return Response::json([]);
-//});
-//
-//Route::get('/api/broadcast/tourney/score', function () {
-//    $match = EventMatch::with(['one.team.logo', 'two.team.logo'])
-//        ->whereNull('winner_id')
-//        ->where([
-//            'matchable_type' => 'tourney'
-//        ])
-//        ->orderBy('takes_place_during', 'asc')
-//        ->first();
-//
-//    $casters = BroadcastCasters::find(1);
-//
-//    return Response::json(['match' => $match, 'casters' => $casters->names]);
-//});
-//
-//Route::get('/api/broadcast/rising/stages', function () {
-//    $event = Event::whereNull('completed')
-//        ->orWhere('completed', 0)
-//        ->with(['matches' => function ($q) {
-//            $q->orderBy('order', 'asc');
-//            $q->with(['claws', 'fangs']);
-//        }])
-//        ->orderBy('id', 'asc')
-//        ->first();
-//
-//    $points = Team::where('qualified', '=', 1)
-//        ->orderBy('points', 'desc')
-//        ->take(3)
-//        ->get();
-//
-//    $activeMatch = $event->matches->where('id', '=', $event->active_match)->first();
-//
-//    return Response::json([
-//        'event' => $event,
-//        'points' => $points,
-//        'highlight' => $activeMatch
-//    ]);
-//});
-//
-//Route::get('/api/broadcast/rising/scoreboard', function () {
-//    $id = Event::orderBy('id', 'desc')
-//        ->first()->active_match;
-//
-//    $match = Match::with(['claws', 'fangs'])
-//        ->where([
-//            'id' => $id
-//        ])
-//        ->first();
-//
-//    $casters = BroadcastCasters::find(1);
-//
-//    return Response::json(['match' => $match, 'casters' => $casters->names]);
-//});
+Route::get('/api/broadcast/{broadcast}/bracket', function ($broadcastId)
+{
+    $broadcast = Broadcast::where(['id' => $broadcastId])
+        ->first();
+
+    if (isset($broadcast->active_match)) {
+        $event = Event::whereId($broadcast->event_id)
+            ->with(['winner', 'matches' => function($q)
+            {
+                $q->with(['one.logo', 'two.logo']);
+            }])
+            ->first();
+
+        $placement = $event->teams()->orderBy('pivot_placement')->get();
+
+        return Response::json([
+            'event' => $event,
+            'broadcast' => $broadcast,
+            'placement' => $placement
+        ]);
+    }
+
+    return Response::json([]);
+})->where(['broadcast' => '[0-9]+']);
