ac1c4133de3dc17eb92f729f002857f7f750827e
Paul Lovato <plovato@gmail.com>
2019-05-05 10:11:34 -0600

    APIs working.

--- routes.php
+++ routes.php
@@ -45,3 +45,42 @@ Route::get('/api/broadcast/{broadcast}/bracket', function ($broadcastId)
 
     return Response::json([]);
 })->where(['broadcast' => '[0-9]+']);
+
+Route::get('/api/broadcast/{broadcast}/group', function ($broadcastId)
+{
+    $broadcast = Broadcast::where(['id' => $broadcastId])
+        ->first();
+
+    if (isset($broadcast->active_match)) {
+        $event = Event::where(['id' => $broadcast->event_id])
+            ->with(['teams', 'winner', 'matches' => function($q) use ($broadcast)
+            {
+                $q->where(['takes_place_during' => $broadcast->roundRobinGroup()->takes_place_during]);
+                $q->with(['one.logo', 'two.logo']);
+            }])
+            ->first();
+
+        $groupTeams = [];
+        foreach ($event->matches as $match) {
+            $groupTeams[] = $match->one->id;
+            $groupTeams[] = $match->two->id;
+        }
+        $groupTeams = array_unique($groupTeams);
+
+        $eventTeams = [];
+        foreach ($event->teams()->orderByDesc('pivot_placement')->get() as $team) {
+            if (in_array($team->id, $groupTeams)) {
+                $eventTeams[] = $team;
+            }
+        }
+
+        return Response::json([
+            'event' => $event,
+            'broadcast' => $broadcast,
+            'placement' => $eventTeams,
+            'group_number' => $event->matches[0]->takes_place_during
+        ]);
+    }
+
+    return Response::json([]);
+})->where(['broadcast' => '[0-9]+']);
